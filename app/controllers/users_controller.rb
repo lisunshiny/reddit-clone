5c2e29de96f2274edd39daa273d038e9ff45cd99
sj+liann <sj_liann@appacademy.io>
2015-04-17 11:12:06 -0400

    Create UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,45 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    if user.save
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
   end
 
 
   def new
+    @user = User.new
+
+    render :new
   end
 
   def edit
+    @user = User.find(params[:id])
+
+    render :edit
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to new_user_url
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_url(@user)
   end
 
   def show
+    @user = User.find(params[:id])
+
+    render :show
   end
 
   private
