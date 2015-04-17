242ba45915eb08fcec3d5c2047e1c6ff1a9e68d5
sj+liann <sj_liann@appacademy.io>
2015-04-17 11:30:13 -0400

    Create SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(session_params)
+    if @user
+      log_in_user(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def destroy
+    log_out_user
+    redirect_to new_session_url
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, password)
+    end
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if user.save
+    if @user.save
+      log_in_user(@user)
       redirect_to user_url(@user)
     else
       render :new
